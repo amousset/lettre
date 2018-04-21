5022a4e76c7b3afbe8deea6c4033c049000949f3
Alexis Mousset <alexis.mousset@normation.com>
2018-04-21 04:08:39 +0200

    Rework internal structs

--- lettre/benches/transport_smtp.rs
+++ lettre/benches/transport_smtp.rs
@@ -4,7 +4,7 @@ extern crate lettre;
 extern crate test;
 
 use lettre::{ClientSecurity, SmtpTransport};
-use lettre::{EmailAddress, EmailTransport, SimpleSendableEmail};
+use lettre::{EmailAddress, SendableEmail, Transport};
 use lettre::smtp::ConnectionReuseParameters;
 
 #[bench]
@@ -13,7 +13,7 @@ fn bench_simple_send(b: &mut test::Bencher) {
         .unwrap()
         .build();
     b.iter(|| {
-        let email = SimpleSendableEmail::new(
+        let email = SendableEmail::new(
             EmailAddress::new("user@localhost".to_string()),
             vec![EmailAddress::new("root@localhost".to_string())],
             "id".to_string(),
@@ -31,7 +31,7 @@ fn bench_reuse_send(b: &mut test::Bencher) {
         .connection_reuse(ConnectionReuseParameters::ReuseUnlimited)
         .build();
     b.iter(|| {
-        let email = SimpleSendableEmail::new(
+        let email = SendableEmail::new(
             EmailAddress::new("user@localhost".to_string()),
             vec![EmailAddress::new("root@localhost".to_string())],
             "id".to_string(),
